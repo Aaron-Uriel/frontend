8f64cb150043fcc15ca57901d22d47045c2b2a09
Aaron-Uriel <therialaaronuriel@gmail.com>
2021-07-20 19:08:48 -0500

    Ahora la app puede pedir cuantas mesas hay desde el servidor

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 const double title_size = 30;
 const double button_text_size = 35;
-const String base_url = 'http://192.168.1.7:8080';
+const String base_url = 'http://192.168.1.7:8080/';
+
+final BackendConnector myServer = BackendConnector(base_url);
 
 void main() {
   runApp(MyApp());
@@ -33,6 +37,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<Table>> tablesList;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -51,20 +57,30 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.50,
-              width: MediaQuery.of(context).size.width * 0.80,
-              child: GridView.count(
-                crossAxisCount: 3,
-                children: List.generate(18, (index) {
-                  return TextButton(
-                    child: Text(
-                      '${index + 1}',
-                      style: TextStyle(fontSize: button_text_size),
-                    ),
-                    onPressed: null,
-                  );
-                }),
+            Center(
+              child: FutureBuilder<List<Table>>(
+                future: tablesList,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final List<Table> tables_list = snapshot.data!;
+                    return SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.50,
+                      width: MediaQuery.of(context).size.width * 0.80,
+                      child: GridView.count( 
+                        crossAxisCount: 3,
+                        children: List.generate(tables_list.length, (index) {
+                          return TextButton(
+                            onPressed: null,
+                            child: Text('${index + 1}', style: TextStyle(fontSize: button_text_size)),
+                          );
+                        }),
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  return CircularProgressIndicator();
+                },
               ),
             ),
           ],
@@ -72,14 +88,50 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+  @override
+  void initState() {
+    super.initState();
+    tablesList = myServer.askForTablesInfo();
+  }
+}
+
+
+class BackendConnector {
+  final String baseUrl;
+
+  //Constructor
+  BackendConnector(this.baseUrl);
+
+  Future<List<Table>> askForTablesInfo() async {
+    final response =
+      await http.get(Uri.parse(this.baseUrl + 'tables_list'));
+    if (response.statusCode == 200) {
+      final Iterable rawList = json.decode(response.body);
+      return List<Table>.from(rawList.map((table) => Table.fromJson(table)));
+    } else {
+      throw Exception('Failed to load tables list');
+    }
+  }
 }
 
 class Table {
   final int id;
-  final bool is_occupied;
+  final bool isOccupied;
 
   Table({
     required this.id,
-    required this.is_occupied,
+    required this.isOccupied
   });
+
+  void printme() {
+    print("Table =  id: ${this.id}, isOccupied: ${this.isOccupied}");
+  }
+
+  factory Table.fromJson(Map<String, dynamic> json) {
+    return Table(
+      id: json['id'],
+      isOccupied: json['is_occupied']
+    );
+  }
 }
